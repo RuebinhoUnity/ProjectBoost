920e42b3c4487a1d2fff65f4625131a1fc0f3b42
Dennis Rübner <dennis.ruebner@gmail.com>
2019-01-14 16:41:50 +0100

    Vorbereitung für Bewegung von Levelteilen

---
+++ ProjectBoost/Assets/Environment/Oscillator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+
+    //todo remove from inspector later
+    [Range(0,1)] [SerializeField] float movementFactor;
+
+
+    private Vector3 startingPosition;
+
+	// Use this for initialization
+	void Start () {
+
+        startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+	}
+}
